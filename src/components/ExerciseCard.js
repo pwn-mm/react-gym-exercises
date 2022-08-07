e34474f6d2e71882a799be7a705e7878e1064586
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-08-07 19:30:16 +0630

    Bug fixed

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
 
 const ExerciseCard = () => {
-  return (
-    <div>ExerciseCard</div>
-    <p>Code Reviewed</p>
-  )
-}
+  return <div>ExerciseCard</div>;
+};
 
-export default ExerciseCard
+export default ExerciseCard;
